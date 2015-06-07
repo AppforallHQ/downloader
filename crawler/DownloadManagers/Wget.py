379dd0ef86b7ae418c8b556993b09382b77c4916
Bersam Karbasion <bersam.k@gmail.com>
2015-06-07 14:35:45 +0430

    init Wget.py DownlodManager

---
+++ crawler/DownloadManagers/Wget.py
@@ -0,0 +1,32 @@
+from .DownloadManager import DownloadManager
+from subprocess import Popen, PIPE
+from time import sleep
+from fcntl import fcntl, F_GETFL, F_SETFL
+from os import O_NONBLOCK, read, path, remove
+import logging,re
+
+
+class Wget(DownloadManager):
+    def __init__(self):
+        super(DownloadManager,self).__init__()
+        self.procparam = ["wget"]
+        self.cookies={}
+        self.postdata={}
+        self.link = ""
+        self.logger = logging.getLogger(__name__)
+
+    def SetParameter(self,lst):
+        for param in lst:
+            self.procparam.append(param)
+
+    def SetCookie(self,key,value):
+        self.cookies[key] = value
+
+    def PostData(self,key,value):
+        self.postdata[key]=value
+
+    def SetLink(self,link):
+        self.link = link
+
+    def StartDownload(self,wd):
+        pass
