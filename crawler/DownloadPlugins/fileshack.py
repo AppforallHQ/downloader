88f51c195ef1d28bb2ea60ee7c2a85cd4b50ce70
Bersam Karbasion <bersam.k@gmail.com>
2015-07-12 13:43:16 +0430

    adding fileshack downloadplugin

---
+++ crawler/DownloadPlugins/fileshack.py
@@ -0,0 +1,40 @@
+from .DownloadPlugin import DownloadPlugin
+import logging, os, requests
+from bs4 import BeautifulSoup as Soup
+import settings
+
+class TurboBit(DownloadPlugin):
+    def __init__(self):
+        super(DownloadPlugin,self).__init__()
+        self.logger = logging.getLogger(__name__)
+        self.cookies = {}
+        self.time = 0
+
+    def canDownload(self,link):
+        return link.find("fileshack.net") != -1
+
+    def HandleDownload(self,link,wd,dlmanager):
+        try:
+            download_id = link.split("/")[-1]
+            post_params = "op=download2&id="+download_id
+            filename = None
+            try:
+                req = requests.get(link, cookies=self.cookies)
+                soup = Soup(req.text)
+                filename = soup.find("span", {"class": "dfilename"}).text
+            except Exception as err:
+                self.logger.error("Error fetching file name : %s" % err)
+                pass
+            if filename:
+                dlmanager.SetParameter(["-o", filename])
+            if post_params:
+                dlmanager.SetParameter(["--post-data", post_params])
+            dlmanager.SetLink(link)
+
+            if dlmanager.StartDownload(wd) == 0:
+                return os.listdir(wd)[0]
+            else:
+                return None
+        except Exception as e:
+            self.logger.error("Error Downloading : %s" % e)
+        return None
